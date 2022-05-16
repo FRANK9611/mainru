de3bbf4d5b9476e126f9e158f6436843c807321b
FRANK9611 <105096684+FRANK9611@users.noreply.github.com>
2022-05-16 21:12:20 +0900

    Add files via upload
    calendar

---
+++ mainru.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import calendar
+
+text=calendar.calendar(2022)
+
+root=Tk()
+root.geometry("550x600")
+root.title("CALENDAR")
+label1=Label(root,text="CALENDAR",bg="yellow",font=("times",28,'bold'))
+label1.grid(row=1,column=1)
+root.config(background="yellow")
+l1=Label(root,text=text,font="Consolas 10")
+l1.grid(row=2,column=1,padx=20)
+root.mainloop()
